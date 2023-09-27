9f9039913213754c8b69c73f48fcf90960957893
andrew <sdd@yandex.ru>
2023-09-27 19:29:54 +0300

    initial

---
+++ src/Product.java
@@ -0,0 +1,36 @@
+public class Product {
+
+    private String name;
+    private Float price;
+    private Float rating;
+
+    public Product(String name, Float price, Float rating) {
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+}
